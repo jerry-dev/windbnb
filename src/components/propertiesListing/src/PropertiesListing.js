916c6f79962244ba1f5eb034d1b421d7bf187375
JerryD <jerry.webdev@gmail.com>
2021-01-19 14:21:16 -0500

    Fixed the issue of the content overflowing the columns when the window shrinks

--- src/components/propertiesListing/src/PropertiesListing.js
+++ src/components/propertiesListing/src/PropertiesListing.js
@@ -50,7 +50,7 @@ export default class PropertiesListing extends HTMLElement {
                 position: relative;
                 z-index: 5;
                 display: grid;
-                grid-template-columns: repeat(3, minmax(min(10%, 100%), 1fr));
+                grid-template-columns: repeat(3, minmax(min(5%, 100%), 1fr));
                 grid-column-gap: 34px;
                 grid-row-gap: 50px;
             }
